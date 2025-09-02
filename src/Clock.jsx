c8351344b09c0a376c67e039ff39a4b5c258f860
Sham Safarany <125689014+Shamsafarany@users.noreply.github.com>
2025-09-02 14:13:18 +0300

    add style

--- src/Clock.jsx
+++ src/Clock.jsx
@@ -3,7 +3,11 @@ import { useState } from "react";
 function Clock() {
 
     return(<>
-    
+        <div className="mainContainer">
+            <div className="clockContainer">
+                <span>00:00:00</span>
+            </div>
+        </div>
     
     
     </>);
